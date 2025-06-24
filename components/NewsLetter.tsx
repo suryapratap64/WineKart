54fae36b19f9165585aafc869194090338067c2b
Surya Pratap <sps64044@gmail.com>
2025-06-25 01:31:27 +0530

    k

--- components/NewsLetter.tsx
+++ components/NewsLetter.tsx
@@ -5,7 +5,7 @@ export const NewsLetter =() =>{
         <div className=" flex flex-col items-center justify-center text-center space-y-2 pt-7 pb-10">
             <h2 className="md:text-4xl text-2xl font-medium">Subscribe now & get free delivery </h2>
             <p className="md:text-base text-gray-500/80 pb-8">
-            this product is best and less costly as compare to your local theka ssave your privecy and enjoy it in your life with privecy </p>
+            this product is the best and less costly as compare to your local theka ssave your privecy and enjoy it in your life with privecy </p>
              <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
         <input
           className="border border-gray-500/30 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
